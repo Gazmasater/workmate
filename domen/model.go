937ea3e6a28494fcb3de5d0a2ba82161171c46e8
Lev <tolkoplus@gmail.com>
2025-07-05 00:06:00 +0300

    proba

--- domen/model.go
+++ domen/model.go
@@ -9,7 +9,7 @@ const (
 	StatusRunning   Status = "RUNNING"
 	StatusCompleted Status = "COMPLETED"
 	StatusFailed    Status = "FAILED"
-	StatusCancelled Status = "CANCELLED"
+	StatusCancelled Status = "CANCELED"
 )
 
 // swagger:model Task
