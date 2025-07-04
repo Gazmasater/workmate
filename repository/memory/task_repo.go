14b54f3643858a644e6a47873d3d8cd000973dc3
Lev <tolkoplus@gmail.com>
2025-07-05 01:55:06 +0300

    1000 GOROUTIN

--- repository/memory/task_repo.go
+++ repository/memory/task_repo.go
@@ -32,7 +32,7 @@ func (r *InMemoryRepo) Update(task *domen.Task) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	if _, exists := r.tasks[task.ID]; !exists {
-		return errors.New("task not found")
+		return errors.New("not found")
 	}
 	tCopy := *task
 	r.tasks[task.ID] = &tCopy
